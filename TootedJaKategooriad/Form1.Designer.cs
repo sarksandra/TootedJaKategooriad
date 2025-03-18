6de82786a4dd49eafbb9ee8758bfd10e30650e7b
opilane <opilane@HP-CZC321706M>
2025-03-18 11:10:19 +0200

    Add project files.

---
+++ TootedJaKategooriad/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace TootedJaKategooriad
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            dataGridViewCategories = new DataGridView();
+            categoryIDDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            categoryNameDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            categoryBindingSource = new BindingSource(components);
+            dataGridViewProducts = new DataGridView();
+            productIDDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            productNameDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            categoryIDDataGridViewTextBoxColumn1 = new DataGridViewTextBoxColumn();
+            categoryDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            productsBindingSource = new BindingSource(components);
+            buttonSave = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewCategories).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)categoryBindingSource).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewProducts).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)productsBindingSource).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridViewCategories
+            // 
+            dataGridViewCategories.AutoGenerateColumns = false;
+            dataGridViewCategories.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCategories.Columns.AddRange(new DataGridViewColumn[] { categoryIDDataGridViewTextBoxColumn, categoryNameDataGridViewTextBoxColumn });
+            dataGridViewCategories.DataSource = categoryBindingSource;
+            dataGridViewCategories.Location = new Point(12, 12);
+            dataGridViewCategories.Name = "dataGridViewCategories";
+            dataGridViewCategories.Size = new Size(385, 372);
+            dataGridViewCategories.TabIndex = 0;
+            dataGridViewCategories.SelectionChanged += dataGridViewCategories_SelectionChanged;
+            // 
+            // categoryIDDataGridViewTextBoxColumn
+            // 
+            categoryIDDataGridViewTextBoxColumn.DataPropertyName = "CategoryID";
+            categoryIDDataGridViewTextBoxColumn.HeaderText = "CategoryID";
+            categoryIDDataGridViewTextBoxColumn.Name = "categoryIDDataGridViewTextBoxColumn";
+            categoryIDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // categoryNameDataGridViewTextBoxColumn
+            // 
+            categoryNameDataGridViewTextBoxColumn.DataPropertyName = "CategoryName";
+            categoryNameDataGridViewTextBoxColumn.HeaderText = "CategoryName";
+            categoryNameDataGridViewTextBoxColumn.Name = "categoryNameDataGridViewTextBoxColumn";
+            // 
+            // categoryBindingSource
+            // 
+            categoryBindingSource.DataSource = typeof(Category);
+            // 
+            // dataGridViewProducts
+            // 
+            dataGridViewProducts.AutoGenerateColumns = false;
+            dataGridViewProducts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewProducts.Columns.AddRange(new DataGridViewColumn[] { productIDDataGridViewTextBoxColumn, productNameDataGridViewTextBoxColumn, categoryIDDataGridViewTextBoxColumn1, categoryDataGridViewTextBoxColumn });
+            dataGridViewProducts.DataSource = productsBindingSource;
+            dataGridViewProducts.Location = new Point(403, 12);
+            dataGridViewProducts.Name = "dataGridViewProducts";
+            dataGridViewProducts.Size = new Size(385, 372);
+            dataGridViewProducts.TabIndex = 1;
+            // 
+            // productIDDataGridViewTextBoxColumn
+            // 
+            productIDDataGridViewTextBoxColumn.DataPropertyName = "ProductID";
+            productIDDataGridViewTextBoxColumn.HeaderText = "ProductID";
+            productIDDataGridViewTextBoxColumn.Name = "productIDDataGridViewTextBoxColumn";
+            productIDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // productNameDataGridViewTextBoxColumn
+            // 
+            productNameDataGridViewTextBoxColumn.DataPropertyName = "ProductName";
+            productNameDataGridViewTextBoxColumn.HeaderText = "ProductName";
+            productNameDataGridViewTextBoxColumn.Name = "productNameDataGridViewTextBoxColumn";
+            // 
+            // categoryIDDataGridViewTextBoxColumn1
+            // 
+            categoryIDDataGridViewTextBoxColumn1.DataPropertyName = "CategoryID";
+            categoryIDDataGridViewTextBoxColumn1.HeaderText = "CategoryID";
+            categoryIDDataGridViewTextBoxColumn1.Name = "categoryIDDataGridViewTextBoxColumn1";
+            categoryIDDataGridViewTextBoxColumn1.Visible = false;
+            // 
+            // categoryDataGridViewTextBoxColumn
+            // 
+            categoryDataGridViewTextBoxColumn.DataPropertyName = "Category";
+            categoryDataGridViewTextBoxColumn.HeaderText = "Category";
+            categoryDataGridViewTextBoxColumn.Name = "categoryDataGridViewTextBoxColumn";
+            categoryDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // productsBindingSource
+            // 
+            productsBindingSource.DataMember = "Products";
+            productsBindingSource.DataSource = categoryBindingSource;
+            // 
+            // buttonSave
+            // 
+            buttonSave.Location = new Point(713, 404);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(75, 23);
+            buttonSave.TabIndex = 2;
+            buttonSave.Text = "Salvesta";
+            buttonSave.UseVisualStyleBackColor = true;
+            buttonSave.Click += buttonSave_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonSave);
+            Controls.Add(dataGridViewProducts);
+            Controls.Add(dataGridViewCategories);
+            Name = "MainForm";
+            Text = "Tooted ja kategooria";
+            ((System.ComponentModel.ISupportInitialize)dataGridViewCategories).EndInit();
+            ((System.ComponentModel.ISupportInitialize)categoryBindingSource).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewProducts).EndInit();
+            ((System.ComponentModel.ISupportInitialize)productsBindingSource).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridViewCategories;
+        private DataGridView dataGridViewProducts;
+        private Button buttonSave;
+        private BindingSource categoryBindingSource;
+        private BindingSource productsBindingSource;
+        private DataGridViewTextBoxColumn categoryIDDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn categoryNameDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn productIDDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn productNameDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn categoryIDDataGridViewTextBoxColumn1;
+        private DataGridViewTextBoxColumn categoryDataGridViewTextBoxColumn;
+    }
+}
---
+++ TootedJaKategooriad/ProductsContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TootedJaKategooriad
+{
+    public class ProductsContext : DbContext
+    {
+        public DbSet<Product> Products { get; set;}
+        public DbSet<Category> Categories { get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite("Data Source=product.db");
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryID = 1, CategoryName = "Dog"},
+                new Category { CategoryID = 2, CategoryName = "Cat"},
+                new Category { CategoryID = 3, CategoryName = "Bunny"},
+                new Category { CategoryID = 4, CategoryName = "Nothing"}
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product { ProductID = 1, CategoryID = 1, ProductName = "Affenpinscher " },
+                new Product { ProductID = 2, CategoryID = 1, ProductName = "Afghan Hound " },
+                new Product { ProductID = 3, CategoryID = 1, ProductName = "Airedale Terrier " },
+                new Product { ProductID = 4, CategoryID = 1, ProductName = "Alaskan Malamute " },
+                new Product { ProductID = 5, CategoryID = 1, ProductName = "American Bulldog " },
+                new Product { ProductID = 6, CategoryID = 1, ProductName = "American Cocker Spaniel " },
+                new Product { ProductID = 7, CategoryID = 1, ProductName = "Anatolian Shepherd Dog " },
+                new Product { ProductID = 8, CategoryID = 1, ProductName = "Australian Cattle Dog " },
+                new Product { ProductID = 9, CategoryID = 1, ProductName = "Australian Shepherd Dog " },
+                new Product { ProductID = 10, CategoryID = 1, ProductName = "Australian Silky Terrier " },
+                new Product { ProductID = 11, CategoryID = 1, ProductName = "Australian Terrier " },
+                new Product { ProductID = 12, CategoryID = 1, ProductName = "Azawakh " },
+                new Product { ProductID = 13, CategoryID = 1, ProductName = "Barbet " },
+                new Product { ProductID = 14, CategoryID = 1, ProductName = "Basenji " },
+                new Product { ProductID = 15, CategoryID = 1, ProductName = "Bloodhound " },
+
+                new Product { ProductID = 16, CategoryID = 2, ProductName = "Abyssinian " },
+                new Product { ProductID = 17, CategoryID = 2, ProductName = "American Bobtail " },
+                new Product { ProductID = 18, CategoryID = 2, ProductName = "Aegean " },
+                new Product { ProductID = 19, CategoryID = 2, ProductName = "American Curl " },
+                new Product { ProductID = 20, CategoryID = 2, ProductName = "American Longhair " },
+                new Product { ProductID = 21, CategoryID = 2, ProductName = "Balinese " },
+                new Product { ProductID = 22, CategoryID = 2, ProductName = "Asian Semi-Longhair " },
+                new Product { ProductID = 23, CategoryID = 2, ProductName = "Bengal"},
+                new Product { ProductID = 24, CategoryID = 2, ProductName = "Bombay"},
+                new Product { ProductID = 25, CategoryID = 2, ProductName = "Brazilian Shorthair"},
+                new Product { ProductID = 26, CategoryID = 2, ProductName = "British Longhair"},
+                new Product { ProductID = 27, CategoryID = 2, ProductName = "salmiak cats"},
+
+                new Product { ProductID = 28, CategoryID = 3, ProductName = "Alaskan" },
+                new Product { ProductID = 29, CategoryID = 3, ProductName = "Checkered Giant"},
+                new Product { ProductID = 30, CategoryID = 3, ProductName = "American Sable"},
+
+                new Product { ProductID = 31, CategoryID = 4, ProductName ="Nothing"},
+                new Product { ProductID = 32, CategoryID = 4, ProductName ="0"},
+                new Product { ProductID = 33, CategoryID = 4, ProductName ="..."},
+                new Product { ProductID = 34, CategoryID = 4, ProductName ="Pool krõpsupakki"}
+                );
+        }
+    }
+}
